0522e5cd36c9fc3c27016caf864ffbfaa0f63106
SthefanyPereira <272940@unifio.edu.br>
2026-09-14 23:25:14 +0000

    aula14/09

---
+++ ecommerce/src/main/resources/import.sql
@@ -0,0 +1,7 @@
+insert into categoria (nome, descricao) values ('Livros', 'Livros Técnicos');
+
+insert into categoria (nome, descricao) values ('Eletrõnicos', 'Equipamentos Eletrônicos');
+
+insert into categoria (nome, descricao) values ('Escritório', 'Material de Escritório');
+
+insert into produto (nome, descricao, preco, estoque, categoria_id) values ('Código Limpo', 'Livro do Autor Robert Martin', 73.44, 20, 1);
